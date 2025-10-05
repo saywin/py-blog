e8761778253b4c6e27f527742a4f17a50d66f9d0
Artem <stilon670@gmail.com>
2025-10-05 13:57:41 +0300

    Add models to blog app

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class Post(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="posts")
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    created_time = models.DateTimeField(auto_now_add=True, db_index=True)
+
+    class Meta:
+        ordering = ["-created_time"]
+
+    def __str__(self):
+        return f"Post: '{self.title}'. Owner - {self.owner.username}"
+
+
+class Commentary(models.Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commentaries"
+    )
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="commentaries"
+    )
+    created_time = models.DateTimeField(auto_now_add=True, db_index=True)
+    content = models.TextField()
+
+    def __str__(self):
+        return f"({self.created_time}) - {self.post}, writer: {self.user.username}"
+
+    class Meta:
+        ordering = ["-created_time"]
+        verbose_name_plural = "Commentaries"
