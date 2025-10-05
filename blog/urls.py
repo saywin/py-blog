9c240c763ad33840fd045c5a72c360b257f89c28
Artem <stilon670@gmail.com>
2025-10-05 17:15:47 +0300

    Add paths index and post-detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
+from blog.views import index, PostDetailView
+
 app_name = "blog"
 
 urlpatterns = [
-
+    path("", index, name="index"),
+    path("<int:pk>/", PostDetailView.as_view(), name="post-detail"),
 ]
